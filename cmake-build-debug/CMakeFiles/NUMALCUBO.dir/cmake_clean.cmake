68f619d376515a3ba451f1def2f115d02c9a041f
ChristopherCagua <caguachristopher@gmail.com>
2021-06-28 13:57:18 -0500

    Calcular el cubo de un numero

---
+++ cmake-build-debug/CMakeFiles/NUMALCUBO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NUMALCUBO.dir/main.cpp.obj"
+  "NUMALCUBO.exe"
+  "NUMALCUBO.exe.manifest"
+  "NUMALCUBO.pdb"
+  "libNUMALCUBO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NUMALCUBO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
